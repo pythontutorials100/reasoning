9116b4b58bda195120eb01746710a8827731b295
pythontutorials100 <systems.arw@gmail.com>
2024-11-11 14:53:36 -0800

    Update res3.py

--- main/doc/res3.py
+++ main/doc/res3.py
@@ -21,3 +21,46 @@ WHERE {
   ?subject ?predicate ?object .
   FILTER (?subject != owl:Thing && ?object != owl:Thing && ?subject != owl:Class && ?object != owl:Class)
 }
+
+
+=======
+
+PREFIX asd: <http://example.org/asd#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+CONSTRUCT {
+  ?subject ?predicate ?object .
+}
+WHERE {
+  ?subject ?predicate ?object .
+  FILTER (
+    ?subject != owl:Thing &&
+    ?object != owl:Thing &&
+    ?subject != owl:Class &&
+    ?object != owl:Class
+  )
+  FILTER NOT EXISTS { ?subject a rdf:Property . }
+  FILTER NOT EXISTS { ?object a rdf:Property . }
+}
+
+
+===========
+
+PREFIX asd: <http://example.org/asd#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+
+CONSTRUCT {
+  ?subject ?predicate ?object .
+}
+WHERE {
+  ?subject ?predicate ?object .
+  FILTER (
+    ?subject != owl:Thing &&
+    ?object != owl:Thing &&
+    ?subject != owl:Class &&
+    ?object != owl:Class &&
+    ?subject != asd:precedes &&
+    ?object != asd:precedes
+  )
+}
